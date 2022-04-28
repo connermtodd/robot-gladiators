155e58a27cd51309ef8502ae74750393cf562008
connermtodd <99088964+connermtodd@users.noreply.github.com>
2022-04-27 21:26:39 -0400

    startGame and endGame functions for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -53,12 +53,21 @@ var fight = function(enemyName) {
   }
 };
 
-for (var i = 0; i < enemyNames.length; i++) {
+var startGame = function() {
+  //reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+
+  for (var i = 0; i < enemyNames.length; i++) {
 
   if (playerHealth > 0) {
     window.alert('Welcome to Robot Gladiators! Round ' + (i + 1));
+    
     var pickedEnemyName = enemyNames[i];
+    
     enemyHealth = 50;
+    
     fight(pickedEnemyName);
   }
   else {
@@ -66,3 +75,28 @@ for (var i = 0; i < enemyNames.length; i++) {
     break;
   }
 }
+
+endGame();
+};
+
+var endGame = function() {
+  window.alert("The game has now ended! Let's see how you did.");
+
+  if (playerHealth >0) {
+    window.alert("Great job, you've survived the game. You now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    startGame();
+  }
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
+
+startGame();
